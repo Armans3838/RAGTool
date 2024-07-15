41c537171886c104e8b5bcc76a1ae2440b1e438f
Armans3838 <armans3838@gmail.com>
2024-07-15 09:58:13 -0700

    working to get LlamaCPPworking with RAG application

--- RAG.py
+++ RAG.py
@@ -6,6 +6,12 @@ from llama_index.llms.lmstudio import LMStudio
 from llama_index.core.base.llms.types import ChatMessage, MessageRole
 from llama_index.embeddings.huggingface import HuggingFaceEmbedding
 from llama_index.core.node_parser import (SentenceSplitter, SemanticSplitterNodeParser, SemanticDoubleMergingSplitterNodeParser, LanguageConfig)
+from llama_index.llms.llama_cpp import LlamaCPP
+from llama_index.llms.llama_cpp.llama_utils import (messages_to_prompt, completion_to_prompt)
+from llama_cpp import Llama
+from llama_index.core import set_global_tokenizer
+from transformers import AutoTokenizer
+
 
 
 # global variables
@@ -21,7 +27,8 @@ class RAG:
         data = data_path
         embedding_model = model
 
-        RAG.__set_global_settings()
+        RAG.__set_global_settingsLMStudio()
+        # RAG.__set_global_settingsLlamaCPP()
 
 
 
@@ -38,7 +45,7 @@ class RAG:
 
 
     # Set the global settings for LlamaIndex
-    def __set_global_settings():
+    def __set_global_settingsLMStudio():
         Settings.embed_model = HuggingFaceEmbedding(model_name=embedding_model)
 
         Settings.llm = LMStudio(
@@ -47,6 +54,41 @@ class RAG:
             temperature=0.7, # changing this value will change the randomness of the responses with 0 being deterministic and 1 being completely random
             request_timeout=60
         )
+
+
+    # Working on allowing this program to be run with LlamaCPP_Python as opposed to LlamaStudio alone. Not done yet.
+    # def __set_global_settingsLlamaCPP():
+    #     Settings.embed_model = HuggingFaceEmbedding(model_name=embedding_model, trust_remote_code=True)
+
+    #     # llm = Llama.from_pretrained(
+    #     #     repo_id="lmstudio-community/Meta-Llama-3-8B-Instruct-BPE-fix-GGUF",
+    #     #     filename="Meta-Llama-3-8B-Instruct-Q5_K_M.gguf",
+    #     #     verbose=False
+    #     # )
+
+    #     set_global_tokenizer(AutoTokenizer.from_pretrained("openbmb/MiniCPM-Llama3-V-2_5").encode)
+
+    #     Settings.llm = LlamaCPP(
+    #         # You can pass in the URL to a GGML model to download it automatically
+    #         model_url="https://huggingface.co/openbmb/MiniCPM-Llama3-V-2_5/tree/main",
+    #         # optionally, you can set the path to a pre-downloaded model instead of model_url
+    #         model_path=None,
+    #         temperature=0.7,
+    #         max_new_tokens=256,
+    #         # llama2 has a context window of 4096 tokens, but we set it lower to allow for some wiggle room
+    #         context_window=3900,
+    #         # kwargs to pass to __call__()
+    #         generate_kwargs={},
+    #         # kwargs to pass to __init__()
+    #         # set to at least 1 to use GPU
+    #         model_kwargs={"n_gpu_layers": 8},
+    #         # transform inputs into Llama2 format
+    #         messages_to_prompt=messages_to_prompt,
+    #         completion_to_prompt=completion_to_prompt,
+    #         verbose=True,
+    #         # request_timeout=60
+    #     )
+
 
 
     # This function creates the index for a given embedding model and stores it in persistent storage
